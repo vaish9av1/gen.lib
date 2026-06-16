37b5366311c409fd64c83cde13f9b6ce8c5943bc
vaish9av1 <vaishnavttr400@gmail.com>
2026-06-16 13:15:17 +0530

    Update settings.py

--- library_system/library_system/settings.py
+++ library_system/library_system/settings.py
@@ -10,9 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     DEBUG=(bool, False)
 )
-environ.Env.read_env(BASE_DIR / '.env')
-if os.path.exists():
-    environ.Env.read_env(env_file)
+if (BASE_DIR / '.env').exists():
+    environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
