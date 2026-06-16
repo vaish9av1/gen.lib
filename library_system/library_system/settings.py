59299d19a7c77a4155274ea51da3b078df9348a8
vaish9av1 <vaishnavttr400@gmail.com>
2026-06-16 11:19:07 +0530

    Update settings.py

--- library_system/library_system/settings.py
+++ library_system/library_system/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','gen-lib.onrender.com'    ]
 
 
 
