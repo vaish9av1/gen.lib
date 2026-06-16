3b569c312d4756111234ff67dd14508f798d8b44
vaish9av1 <vaishnavttr400@gmail.com>
2026-06-16 11:11:13 +0530

    Update settings.py

--- library_system/library_system/settings.py
+++ library_system/library_system/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #AUTH_USER_MODEL = 'accounts.User'
 MEDIA_URL = '/media/'
