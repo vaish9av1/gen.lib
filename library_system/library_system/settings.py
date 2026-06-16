14c72c9b354e49d4def289924ed0d27120a7f35f
vaish9av1 <vaishnavttr400@gmail.com>
2026-06-16 11:24:33 +0530

    installed whitenoise

--- library_system/library_system/settings.py
+++ library_system/library_system/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','gen-lib.onrender.com'    ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','gen-lib.onrender.com']
 
 
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
